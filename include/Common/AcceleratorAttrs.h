15cf05a8a3924856feb48096a60af4a70879d9d8
tancheng <tancheng1990@gmail.com>
2025-05-20 06:33:36 +0000

    Tag kernel with accelerator attr for optional lowering

---
+++ include/Common/AcceleratorAttrs.h
@@ -0,0 +1,20 @@
+#ifndef COMMON_ACCELERATOR_ATTRS_H
+#define COMMON_ACCELERATOR_ATTRS_H
+
+#include "llvm/ADT/StringRef.h"
+
+namespace mlir {
+namespace accel {
+
+// Common attribute key.
+constexpr llvm::StringRef kAcceleratorAttr = "accelerator";
+
+// Common accelerator targets.
+constexpr llvm::StringRef kNeuraTarget = "neura";
+constexpr llvm::StringRef kGpuTarget   = "gpu";
+constexpr llvm::StringRef kTpuTarget   = "tpu";
+
+} // namespace accel
+} // namespace mlir
+
+#endif // COMMON_ACCELERATOR_ATTRS_H
