df7082433691f4023c272ef2806ae531bc9f1e2e
tancheng <tancheng1990@gmail.com>
2025-06-17 07:54:32 +0000

    Provide grant_predicate and calculate RecMII

--- include/NeuraDialect/NeuraPasses.h
+++ include/NeuraDialect/NeuraPasses.h
@@ -22,6 +22,7 @@ std::unique_ptr<mlir::Pass> createFusePatternsPass();
 std::unique_ptr<mlir::Pass> createAssignAcceleratorPass();
 std::unique_ptr<mlir::Pass> createTransformCtrlToDataFlowPass();
 std::unique_ptr<mlir::Pass> createLeveragePredicatedValuePass();
+std::unique_ptr<mlir::Pass> createMapToAcceleratorPass();
 
 #define GEN_PASS_REGISTRATION
 #include "NeuraDialect/NeuraPasses.h.inc"
