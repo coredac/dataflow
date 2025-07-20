3a88f1358d72bcf38ca229b98c9373030ca8f30d
n0thingNoob <yshi16@wm.edu>
2025-07-20 16:57:53 -0400

    Add Generated_Code folder with test results and fix GenerateCodePass source direction calculation

---
+++ Generated_Code/generated-instructions.asm
@@ -0,0 +1,36 @@
+PE(0,2):
+{
+    Entry [West, R] => Once {
+        {
+            RETURN, [West, R] -> [Local, R]
+        }
+    }
+}
+
+PE(1,1):
+{
+    Entry [] => Once {
+        {
+            CONSTANT, IMM[1.000000e+00] -> [North, R]
+        }
+    }
+}
+
+PE(1,2):
+{
+    Entry [North, R], [West, R] => Once {
+        {
+            FADD, [North, R], [West, R] -> [West, R]
+        }
+    }
+}
+
+PE(2,2):
+{
+    Entry [] => Once {
+        {
+            CONSTANT, IMM[2.000000e+00] -> [West, R]
+        }
+    }
+}
+
