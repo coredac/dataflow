b3a24e78bc52e8b5953c47cebc5f41cdff28df21
ShangkunLI <shangkun.li@connect.ust.hk>
2025-07-31 23:58:44 +0800

    [fix] revise the matching pattern

--- lib/NeuraDialect/Transforms/FusePatternsPass.cpp
+++ lib/NeuraDialect/Transforms/FusePatternsPass.cpp
@@ -16,6 +16,8 @@ struct FuseConstantAndGrantPattern
 
   LogicalResult matchAndRewrite(neura::ConstantOp constant_op,
                                 PatternRewriter &rewriter) const override {
+    bool made_change = false;
+
     // Checks if the constant operation is used by a grant_once or grant_always
     // operation.
     for (auto user : constant_op->getUsers()) {
@@ -28,6 +30,7 @@ struct FuseConstantAndGrantPattern
               /*value=*/nullptr, constant_op->getAttr("value"));
           // Replaces the original constant operation with the new one.
           rewriter.replaceOp(grant_once_op, new_grant_once_op);
+          made_change = true;
         } else if (neura::GrantAlwaysOp grant_always_op =
                        dyn_cast<neura::GrantAlwaysOp>(user)) {
           auto new_grant_always_op = rewriter.create<neura::GrantAlwaysOp>(
@@ -35,6 +38,7 @@ struct FuseConstantAndGrantPattern
               /*value=*/nullptr, constant_op->getAttr("value"));
           // Replaces the original constant operation with the new one.
           rewriter.replaceOp(grant_always_op, new_grant_always_op);
+          made_change = true;
         }
       }
     }
@@ -42,9 +46,10 @@ struct FuseConstantAndGrantPattern
     if (constant_op->use_empty()) {
       // If the constant operation has no users, it can be removed.
       rewriter.eraseOp(constant_op);
+      made_change = true;
     }
 
-    return success();
+    return success(made_change);
   }
 };
 
@@ -143,25 +148,49 @@ struct FusePatternsPass
   }
 
   void runOnOperation() override {
-    RewritePatternSet patterns(&getContext());
-    patterns.add<FuseFAddFAddPattern>(&getContext(), 2);
-    patterns.add<FuseFMulFAddPattern>(&getContext(), 3);
-    patterns.add<FuseConstantAndGrantPattern>(&getContext(), 1);
-    FrozenRewritePatternSet frozen(std::move(patterns));
-
     ModuleOp module_op = getOperation();
 
-    // Applies to every region inside the module (regardless of func type,
-    // e.g., mlir func or llvm func).
-    module_op.walk([&](Operation *op) {
-      if (!op->getRegions().empty()) {
-        for (Region &region : op->getRegions()) {
-          if (failed(applyPatternsGreedily(region, frozen))) {
-            signalPassFailure();
-          }
-        }
+    // Phase 1: Apply FuseConstantAndGrantPattern.
+    {
+      RewritePatternSet patterns(&getContext());
+      patterns.add<FuseConstantAndGrantPattern>(&getContext());
+      FrozenRewritePatternSet frozen(std::move(patterns));
+
+      if (failed(applyPatternsGreedily(module_op, frozen))) {
+        signalPassFailure();
       }
-    });
+    }
+
+    // Phase 2: Apply other patterns.
+    {
+      RewritePatternSet patterns(&getContext());
+      patterns.add<FuseFAddFAddPattern>(&getContext());
+      patterns.add<FuseFMulFAddPattern>(&getContext());
+      FrozenRewritePatternSet frozen(std::move(patterns));
+
+      if (failed(applyPatternsGreedily(module_op, frozen))) {
+        signalPassFailure();
+      }
+    }
+    // {
+    //   RewritePatternSet patterns(&getContext());
+    //   patterns.add<FuseFAddFAddPattern>(&getContext(), 2);
+    //   patterns.add<FuseFMulFAddPattern>(&getContext(), 3);
+    //   patterns.add<FuseConstantAndGrantPattern>(&getContext(), 1);
+    //   FrozenRewritePatternSet frozen(std::move(patterns));
+
+    //   // Applies to every region inside the module (regardless of func type,
+    //   // e.g., mlir func or llvm func).
+    //   module_op.walk([&](Operation *op) {
+    //     if (!op->getRegions().empty()) {
+    //       for (Region &region : op->getRegions()) {
+    //         if (failed(applyPatternsGreedily(region, frozen))) {
+    //           signalPassFailure();
+    //         }
+    //       }
+    //     }
+    //   });
+    // }
   }
 };
 
--- test/neura/fadd_fadd.mlir
+++ test/neura/fadd_fadd.mlir
@@ -1,10 +1,10 @@
 // Applies pattern fusion before mov insertion.
-// RUN: mlir-neura-opt --assign-accelerator --lower-arith-to-neura --fuse-patterns --insert-data-mov %s | FileCheck %s
+// RUN: mlir-neura-opt --assign-accelerator --lower-llvm-to-neura --canonicalize-live-in --leverage-predicated-value --transform-ctrl-to-data-flow --fuse-patterns --insert-data-mov %s | FileCheck %s
 
 func.func @test(%a: f32, %b: f32) -> f32 {
-  %c = arith.constant 2.0 : f32
-  %temp = arith.addf %a, %b : f32
-  %res = arith.addf %temp, %c : f32
+  %c = llvm.mlir.constant(2.0 : f32) : f32
+  %temp = llvm.fadd %a, %b : f32
+  %res = llvm.fadd %temp, %c : f32
   // CHECK: neura.fadd_fadd
   return %res : f32
 }
