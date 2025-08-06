db24510c652c5ea54a3d5eb4fc71bd8f9a540c07
Jackcui <bohan.cui@outlook.com>
2025-08-06 12:40:10 -0500

    edit gitignore, reading spec from ENV -> cli parameters

--- lib/NeuraDialect/Transforms/MapToAcceleratorPass.cpp
+++ lib/NeuraDialect/Transforms/MapToAcceleratorPass.cpp
@@ -49,6 +49,12 @@ struct MapToAcceleratorPass
                      "max_loc=5, max_depth=3)"),
       llvm::cl::init("heuristic")};
 
+  Option<std::string> archSpecPath{
+      *this, "arch-spec",
+      llvm::cl::desc("Path to the architecture specification YAML file. "
+                     "If not specified, will use default 4x4 architecture."),
+      llvm::cl::init("")};
+
   void runOnOperation() override {
     ModuleOp module = getOperation();
 
@@ -141,35 +147,36 @@ struct MapToAcceleratorPass
       }
 
       // AcceleratorConfig config{/*numTiles=*/8}; // Example
-      // Read architecture specification from environment variable
-      const char* arch_spec_path = std::getenv("DEFAULT_ARCH_SPEC");
-      // Read from the environment variable for now.
+      // Read architecture specification from command line option
       YAML::Node config;
       bool use_default_arch = false;
-      if (arch_spec_path) {
+      
+      if (!archSpecPath.getValue().empty()) {
         try {
-          std::ifstream file(arch_spec_path);
+          std::ifstream file(archSpecPath.getValue());
           if (file.is_open()) {
             config = YAML::Load(file);
             if (config["architecture"]) {
-              llvm::outs() << "[MapToAcceleratorPass] Loaded architecture from " 
-                          << arch_spec_path << "\n";
+              llvm::outs() << "\033[31m[MapToAcceleratorPass] Loaded architecture from " 
+                          << archSpecPath.getValue() << "\033[0m\n";
             } else {
               llvm::errs() << "[MapToAcceleratorPass] Invalid YAML format in " 
-                          << arch_spec_path << ", using default 4x4\n";
+                          << archSpecPath.getValue() << ", using default 4x4\n";
+              use_default_arch = true;
             }
           } else {
             llvm::errs() << "[MapToAcceleratorPass] Could not open architecture file " 
-                        << arch_spec_path << ", using default 4x4\n";
+                        << archSpecPath.getValue() << ", using default 4x4\n";
             use_default_arch = true;
           }
         } catch (const std::exception& e) {
           llvm::errs() << "[MapToAcceleratorPass] Error parsing YAML file " 
-                      << arch_spec_path << ": " << e.what() << ", using default 4x4\n";
+                      << archSpecPath.getValue() << ": " << e.what() << ", using default 4x4\n";
+          use_default_arch = true;
         }
       } else {
         use_default_arch = true;
-        llvm::errs() << "[MapToAcceleratorPass] DEFAULT_ARCH_SPEC not set, using default 4x4\n";
+        llvm::errs() << "[MapToAcceleratorPass] No architecture specification provided, using default 4x4\n";
       }
 
       Architecture architecture = use_default_arch ? Architecture(4, 4) : Architecture(config);
