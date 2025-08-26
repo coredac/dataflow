1145e8c316dbb024192c931d8d22727fa1ef3a77
Jiajun Qin <89443407+HobbitQia@users.noreply.github.com>
2025-08-26 15:48:47 +0800

    Update visualize.sh

--- test/visualize/visualize.sh
+++ test/visualize/visualize.sh
@@ -1,3 +1,2 @@
-mlir-neura-opt --assign-accelerator --lower-arith-to-neura --fuse-patterns test.mlir > test_fused.mlir
-mlir-opt --allow-unregistered-dialect --view-op-graph test_fused.mlir 2> test.dot 
+mlir-neura-opt --assign-accelerator --lower-arith-to-neura --fuse-pattern --view-op-graph test.mlir 2> test.dot 
 dot -Tpng test.dot -o test.png
