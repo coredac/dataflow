cf30edd03aff6e4fa52be2f4f473435efff126c5
root <root@LAPTOP-VPLF7SOH.localdomain>
2025-09-19 22:34:31 -0700

    Add relu.cpp

--- test/neura/for_loop/relu.cpp
+++ test/neura/for_loop/relu.cpp
@@ -1,39 +1,37 @@
-// RUN: mlir-neura-opt %s | FileCheck %s
-
 #include <stdio.h>
 
 #define N 32
 
-float input[N] = {
-  -1.0, 0.0, 1.0, 2.0, -3.0, 4.0, -5.0, 6.0,
-  -7.0, 8.0, -9.0, 10.0, -11.0, 12.0, -13.0, 14.0,
-  -15.0, 16.0, -17.0, 18.0, -19.0, 20.0, -21.0, 22.0,
-  -23.0, 24.0, -25.0, 26.0, -27.0, 28.0, -29.0, 30.0
+int input[N] = {
+  1, -1, 2, -3, 4, -5, 6, -7,
+  8, -9, 10, -11, 12, -13, 14, -15,
+  16, -17, 18, -19, 20, -21, 22, -23,
+  24, -25, 26, -27, 28, -29, 30, -31
 };
 
-float output[N];
+int output[N];
 
 void kernel(float input[], float output[]);
 
 int main(){
   //init output
-  for(int i=0; i<N; i++){
-    output[i]=0.0;
+  for(int i = 0; i < N; i++){
+    output[i] = 0;
   }
  
   kernel(intput, output);
 
   //print outputs
-  for(int i=0; i<N; i++){
+  for(int i = 0; i < N; i++){
     print("output[%d] = %f\n", i, output[i]); 
   }
 
   return 0;
 }
 
-void kernel(float input[], float output []){
-  for(int i = 0; i<N; ++i){
-    if(input[i]>0){
+void kernel(int input[], int output []){
+  for(int i = 0; i < N; ++i){
+    if(input[i] > 0){
       output[i] += input[i];
     } else {
       output[i] += 0;
