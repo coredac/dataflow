91a36b59ed3b2cc6f9a9515a09465c70cd709c75
ShangkunLI <shangkun.li@connect.ust.hk>
2026-01-03 10:44:33 +0800

    initialize taskflow dialect

---
+++ include/TaskFlowDialect/TaskFlowDialect.h
@@ -0,0 +1,11 @@
+#ifndef TASKFLOW_DIALECT_H
+#define TASKFLOW_DIALECT_H
+
+namespace mlir {
+namespace taskflow {
+class TaskFlowDialect;
+
+#include "TaskFlowDialect/TaskFlowDialect.h.inc"
+} // End namespace taskflow.
+} // End namespace mlir.
+#endif // TASKFLOW_DIALECT_H
---
+++ include/TaskFlowDialect/TaskFlowOps.h
@@ -0,0 +1,1 @@
+// TaskFlowOps.h - TaskFlow dialect operations.
